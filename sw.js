221d07be6a33c87f9fcfd9e48e47d0dd36ad1f4b
Panda-Bey <78523546+Panda-Bey@users.noreply.github.com>
2026-02-16 21:27:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-const C="v3";
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open(C).then(c=>c.addAll([
-  "./","index.html","app.js","manifest.json"
- ])));
+self.addEventListener('install', e=>{
+ e.waitUntil(
+  caches.open('love-cache').then(c=>c.addAll([
+   './',
+   './index.html',
+   './app.js'
+  ]))
+ );
 });
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener('fetch', e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r || fetch(e.request))
+ );
 });
